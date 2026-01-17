e6c38f26597836e63dcf3b96c0068af255b8c299
CHHE CHINYONG <chhechinyong01@gmail.com>
2026-01-17 20:02:52 +0700

    Update DbConnectionFactory to use a new Cloud SQL connection string for the SQL Server instance

--- Data/DbConnectionFactory.cs
+++ Data/DbConnectionFactory.cs
@@ -63,7 +63,7 @@ public class DbConnectionFactory : IDbConnectionFactory
             _logger?.LogWarning(ex, "Could not parse connection string for validation, using as-is");
         }
 
-        _connectionString = connectionString;
+        _connectionString = "/cloudsql/project-8f33c2c1-6350-4a64-90f:asia-southeast1:sql-server-techbodia;Database=NotebookApp;User ID=sqlserver;Password=Helloworld01.;Encrypt=True;TrustServerCertificate=True;";
     }
 
     public IDbConnection CreateConnection()
